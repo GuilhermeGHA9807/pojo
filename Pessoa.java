75799fc0d2138299035743f85b41b5c4727ef987
GuilhermeGHA9807 henrique <guilhermenargasgise@gmail.com>
2025-02-12 17:11:16 +0000

    salvando meu primeiro arquivo

---
+++ Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa{
+    private String nome;
+    private int idade;
+     
+     public void setName( String n){
+     this.name =n;
+     }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setIdade(int i){
+        this.idade = i;
+    }
+public int getIdade(){
+    return this.idade;
+}
+}
